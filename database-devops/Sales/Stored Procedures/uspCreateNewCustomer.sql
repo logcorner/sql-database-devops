26b2a092a0a2d95c0c503a919dc6869f517d5f81
Gora LEYE <leyegora@yahoo.fr>
2020-01-10 20:27:22 +0100

    Init database

---
+++ database-devops/Sales/Stored Procedures/uspCreateNewCustomer.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [Sales].[uspCreateNewCustomer]  
+@CustomerName NVARCHAR (40)  
+AS  
+BEGIN  
+INSERT INTO [Sales].[Customer] (CustomerName) VALUES (@CustomerName);  
+SELECT SCOPE_IDENTITY()  
+END
